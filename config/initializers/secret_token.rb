c3c3b35e58edfda8c13992918b26958466c7b53f
Matt Maloney <mwmaloney88@gmail.com>
2013-06-12 15:32:44 -0400

    initial commit, app setup with postgres and testing suite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoveApp::Application.config.secret_token = '02bea0ef113f387deb44afecc682271cce194604b5aa80e9a0049702df46be7944f55d9074e103d19c92c4395df370b6de0da2d3cc2779691bc01674b454d5bd'
